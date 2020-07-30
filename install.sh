1c1e94ceafddf6430028461221c3cdea7794e524
Spencer Main <spencer.main@bnc.ca>
2020-07-30 09:41:41 -0400

    updates install script to include some autoformatters

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+install_deps() {
+	nvm use 12.18.2
+	npm install -g npm-groovy-lint
+	pip3 install --upgrade autopep8
+}
+
 cat ./keyboard.ascii
 echo -e "		      configuing nvim \n"
 DIR_NVIM="$HOME/.config/nvim"
@@ -9,6 +15,7 @@ if [[ -e $DIR_NVIM ]]; then
 	echo "jumping to $DIR_NVIM"
 	if [[ ! -e $DIR_NVIM/kotlin-language-server ]]; then 
 		git clone https://github.com/fwcd/kotlin-language-server.git && cd ./kotlin-language-server && ./gradlew :server:installDist
+		install_deps
 	fi
 else
 	echo "folder not present...creating it if you let me"
